63587fb841e1b12cd0e94e7a38139add5e813ec2
marsh <marshallkrakauer@gmail.com>
2026-01-22 20:06:29 -0800

    getting the simulations running again

--- game_classes.py
+++ game_classes.py
@@ -9,9 +9,13 @@ class Card:
         self.value = value
         self.charge = charge
         self.is_dodge = is_dodge
+        if self.is_dodge:
+            self.dodge_string = '\n' + 'Dodge'
+        else:
+            self.dodge_string = ''
 
     def __str__(self):
-        'Value: ' + str(self.value) + '\n' + 'Charge:' + self.effect
+        return 'Value: ' + str(self.value) + '\n' + 'Charge:' + str(self.charge) + self.dodge_string
 
 
 class GameDeck:
@@ -27,6 +31,10 @@ class FighterDeck:
     def __init__(self):
         self.card_list = [Card(1), Card(2), Card(3), Card(4), Card(5), Card(6)]
         self.discard = []
+
+    def __repr__(self):
+        for card in self.card_list:
+            print(card)
 
     def print_deck(self):
         print("TOTAL DECK CARDS:", len(self.card_list))
@@ -53,7 +61,7 @@ class FighterDeck:
 
 class Bout:
 
-    def __init__(self, red_corner_deck, blue_corner_deck,  verbose=0, punch_ko_threshold=5,
+    def __init__(self, red_corner_deck, blue_corner_deck,  verbose=1, punch_ko_threshold=5,
                  red_corner_starting_meter = 0, blue_corner_starting_meter = 0):
 
         # Set up Rules of Fight
@@ -64,8 +72,8 @@ class Bout:
         # Set up Starting meter
         # For added clarity, "meter" refers to how much is added/subtracted
         # in current round. "charge" is how much the meter changes next round
-        self.red_corner_starting_meter = red_corner_starting_meter
-        self.blue_corner_starting_meter = blue_corner_starting_meter
+        self.red_corner_meter = red_corner_starting_meter
+        self.blue_corner_meter = blue_corner_starting_meter
 
         # Create decks for each fighter
         self.red_corner_deck = blue_corner_deck
@@ -88,25 +96,31 @@ class Bout:
         self.verbose = verbose
 
 
-    def check_for_ko(self, winner, card_difference):
+    def check_for_ko(self, winner, round_difference):
+        # Draws can't possibly result in KO
+        if self.previous_round_winner == 'draw':
+            return False
+        # Hitting a streak of three rounds is a KO
         if self.previous_round_winner == winner and self.round_streak == 2:
+            self.bout_ended_in_ko = True
+            self.bout_ended_in_tko = True
             return True
-        if card_difference >= self.punch_ko_threshold:
+
+        # Round difference >= threshold is a punch KO
+        if round_difference >= self.punch_ko_threshold:
+            self.bout_ended_in_ko = True
+            self.bout_ended_in_punch_ko = True
             return True
+
+        # If neither threshold met, we have a standard non-KO round
         return False
 
     def handle_victory(self, winner, card_difference):
         """Handle common victory logic for both red_corner and blue_corner wins."""
-
-        if winner == 'draw':
-            is_ko = False
-        else:
-            is_ko = self.check_for_ko(winner, card_difference)
-
-        if is_ko and winner == 'blue_corner':
-            winner = 'blue_corner'
-        if is_ko and winner == 'red_corner':
-            winner = 'red_corner'
+        print("is it making to the handle victory function")
+        # Function updates these values, if possible:
+        # self.bout_ended_in_ko, self.bout_ended_in_punch_ko, self.bout_ended_in_tko
+        round_resulted_in_ko = self.check_for_ko(winner, card_difference)
 
         # Update streak
         if winner != 'draw':
@@ -120,55 +134,69 @@ class Bout:
             self.previous_round_winner = 'draw'
             self.round_streak = 0
 
-        self.previous_round_winner = winner
-
         # Update win counter
         if winner == 'red_corner':
             self.red_corner_wins += 1
         elif winner == 'blue_corner':
             self.blue_corner_wins += 1
 
-        # Check for KO conditions
-        if card_difference >= self.punch_ko_threshold:
-            if self.verbose == 1:
-                print(f"{winner.upper()} PUNCH KO")
-            self.bout_ended_in_ko, self.bout_ended_in_punch_ko = True, True
+        self.previous_round_winner = winner
 
-        if self.round_streak == self.tko_threshold:
-            if self.verbose == 1:
-                print(f"{winner.upper()} TKO")
-            self.bout_ended_in_ko, self.bout_ended_in_tko = True, True
+        # Check for KO conditions
+        if round_resulted_in_ko and self.verbose == 1:
+            if self.bout_ended_in_punch_ko:
+                print(f"{winner.upper()} PUNCH KO")
+            else:
+                print(f"{winner.upper()} TKO KO")
 
     def play_round(self):
-
+        print(self.round_number)
         # Draw Card from each deck
         red_corner_card = self.red_corner_deck.draw_card()
         blue_corner_card = self.blue_corner_deck.draw_card()
 
+        # Does the round involve a dodge. This creates an automated draw
+        round_has_dodge = red_corner_card.is_dodge or blue_corner_card.is_dodge
+
         # Get values on cards. This plus meter will decide winner of round
-        red_corner_value = red_corner_card.value
-        blue_corner_value = blue_corner_card.value
+        red_corner_card_value = red_corner_card.value
+        blue_corner_card_value = blue_corner_card.value
 
         red_corner_charge = red_corner_card.charge
         blue_corner_charge = blue_corner_card.charge
 
-        if self.verbose == 2:
+        if self.verbose == 1:
             print("red_corner:", red_corner_card)
             print("blue_corner:", blue_corner_card)
 
-        # Main logic becomes:
-        if red_corner_card.value > blue_corner_card.value:
-            card_difference = red_corner_value - blue_corner_value
-            self.handle_victory('red_corner', card_difference)
-            round_result = 'R'
-        elif blue_corner_card.value > red_corner_card.value:
-            card_difference = blue_corner_value - red_corner_value
-            self.handle_victory('blue_corner', card_difference)
-            round_result = 'B'
-        else:
+        # Add Meter to Cards
+        red_corner_round_value = red_corner_card_value + self.red_corner_meter
+        blue_corner_round_value = blue_corner_card_value + self.blue_corner_meter
+
+        # Most important_part, check for who won the round
+        if round_has_dodge or red_corner_round_value == blue_corner_round_value:
             round_result = 'D'
             self.previous_round_winner = 'draw'
             self.round_streak = 0
+
+        # Red Corner Wins the Round
+        elif red_corner_round_value > blue_corner_round_value:
+            round_difference = red_corner_round_value - blue_corner_round_value
+            self.handle_victory('red_corner', round_difference)
+            round_result = 'R'
+
+        # Blue Corner Wins the round
+        else:
+            round_difference = blue_corner_round_value - red_corner_round_value
+            self.handle_victory('blue_corner', round_difference)
+            round_result = 'B'
+
+        # Update Meter
+        # Min/Max functions prevent value from going over 2 or under -2
+        # 2 being the default, may be adjusted as game is tested
+
+        self.blue_corner_meter = max(min(self.blue_corner_meter + blue_corner_charge,METER_MAX),METER_MAX * -1)
+        self.red_corner_meter = max(min(self.red_corner_meter + red_corner_charge,METER_MAX),METER_MAX * -1)
 
         self.round_results[self.round_number] = round_result
         self.round_number += 1
--- main.py
+++ main.py
@@ -20,18 +20,18 @@ def process_fight_result(bout, counters, fight_number, verbose=False):
     ko_win = result_of_fight['ko_win']
 
     # Update win counters
-    if winner == 'home':
-        counters['home_win_count'] += 1
+    if winner == 'red_corner':
+        counters['red_corner_win_count'] += 1
         if ko_win == 1:
-            counters['home_ko_count'] += 1
+            counters['red_corner_ko_count'] += 1
         else:
-            counters['home_decision_count'] += 1
-    elif winner == 'away':
-        counters['away_win_count'] += 1
+            counters['red_corner_decision_count'] += 1
+    elif winner == 'blue_corner':
+        counters['blue_corner_win_count'] += 1
         if ko_win == 1:
-            counters['away_ko_count'] += 1
+            counters['blue_corner_ko_count'] += 1
         else:
-            counters['away_decision_count'] += 1
+            counters['blue_corner_decision_count'] += 1
     else:
         counters['draw_count'] += 1
 
@@ -48,17 +48,17 @@ def process_fight_result(bout, counters, fight_number, verbose=False):
     return fight_number + 1
 
 
-def simulate_fights(num_fights=10_000, home_support=0, away_support=0):
+def simulate_fights(num_fights=10_000):
     num_fights = num_fights
     fight_counter = 1
     counters = {
-        'home_win_count': 0,
-        'away_win_count': 0,
+        'red_corner_win_count': 0,
+        'blue_corner_win_count': 0,
         'draw_count': 0,
-        'home_ko_count': 0,
-        'home_decision_count': 0,
-        'away_ko_count': 0,
-        'away_decision_count': 0,
+        'red_corner_ko_count': 0,
+        'red_corner_decision_count': 0,
+        'blue_corner_ko_count': 0,
+        'blue_corner_decision_count': 0,
         'ko_count': 0,
         'tko_count': 0,
         'punch_ko_count': 0,
@@ -67,17 +67,16 @@ def simulate_fights(num_fights=10_000, home_support=0, away_support=0):
     while fight_counter <= num_fights:
         scientist_deck = FighterDeck()
         engineer_deck = FighterDeck()
-        my_bout = Bout(blue_corner_deck=scientist_deck, away_deck=engineer_deck
-                       , home_support_count=home_support
-                       , away_support_count=away_support)
+        #scientist_deck.print_deck()
+        my_bout = Bout(blue_corner_deck=scientist_deck, red_corner_deck=engineer_deck)
         my_bout.fight_bout()
         result_of_fight = my_bout.get_results()
-        process_fight_result(my_bout, counters, fight_counter)
+        process_fight_result(my_bout, counters, fight_counter,True)
         fight_counter += 1
 
     for key, value in counters.items():
-        if key in ['draw_count', 'home_ko_count', 'home_decision_count',
-                   'away_ko_count', 'away_decision_count']:
+        if key in ['draw_count', 'red_corner_ko_count', 'red_corner_decision_count',
+                   'blue_corner_ko_count', 'blue_corner_decision_count']:
             print(key.replace('_', ' '), value)
     print('\n')
 
@@ -86,17 +85,16 @@ def simulate_fights(num_fights=10_000, home_support=0, away_support=0):
 if __name__ == '__main__':
     random.seed(15)
 
-    print("NO SUPPORT")
-    simulate_fights(10_000)
+    simulate_fights(1000)
 
-    print("1 HOME SUPPORT")
-    simulate_fights(10_000, home_support=1)
-
-    print("2 HOME SUPPORT")
-    simulate_fights(10_000, home_support=2)
-
-    print("1 HOME SUPPORT 1 AWAY SUPPORT")
-    simulate_fights(10_000, home_support=1, away_support=1)
-
-    print("2 HOME SUPPORT 2 AWAY SUPPORT")
-    simulate_fights(10_000, home_support=2, away_support=2)
+    # print("1 red_corner SUPPORT")
+    # simulate_fights(10_000)
+    #
+    # print("2 red_corner SUPPORT")
+    # simulate_fights(10_000)
+    #
+    # print("1 red_corner SUPPORT 1 blue_corner SUPPORT")
+    # simulate_fights(10_000)
+    #
+    # print("2 red_corner SUPPORT 2 blue_corner SUPPORT")
+    # simulate_fights(10_000)
